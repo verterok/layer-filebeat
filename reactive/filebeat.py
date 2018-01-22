583f632f2b809530f65e8619c811bc2205fe003c
Ricardo Kirkner <ricardokirkner@gmail.com>
2018-01-22 18:39:01 -0300

    added options to configure ssl support for logstash (#32)
    * added options to configure ssl support for logstash
    
    * state that the ssl data is expected to be provided as a base64-encoded string
    
    * decode ssl cert data when writing to disk
    
    * changed decorator order

--- reactive/filebeat.py
+++ reactive/filebeat.py
@@ -5,15 +5,21 @@ from charms.reactive import when_not
 from charms.reactive import set_state
 from charms.reactive import remove_state
 from charms.reactive import hook
+from charms.templating.jinja2 import render
 
-from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.hookenv import config, status_set
 from charmhelpers.core.host import restart_on_change, service_stop
 
 from elasticbeats import render_without_context
 from elasticbeats import enable_beat_on_boot
 from elasticbeats import push_beat_index
 
+import base64
 import os
+
+
+LOGSTASH_SSL_CERT = '/etc/ssl/certs/filebeat-logstash.crt'
+LOGSTASH_SSL_KEY = '/etc/ssl/private/filebeat-logstash.key'
 
 
 @when_not('apt.installed.filebeat')
@@ -22,14 +28,37 @@ def install_filebeat():
     charms.apt.queue_install(['filebeat'])
 
 
-@restart_on_change('/etc/filebeat/filebeat.yml', ['filebeat'])
 @when('beat.render')
 @when('apt.installed.filebeat')
+@restart_on_change('/etc/filebeat/filebeat.yml', ['filebeat'])
 def render_filebeat_template():
     connections = render_without_context('filebeat.yml', '/etc/filebeat/filebeat.yml')
     remove_state('beat.render')
     if connections:
         status_set('active', 'Filebeat ready.')
+
+
+@when('beat.render')
+@when('apt.installed.filebeat')
+@restart_on_change({
+    LOGSTASH_SSL_CERT: ['filebeat'],
+    LOGSTASH_SSL_KEY: ['filebeat'],
+    })
+def render_filebeat_logstash_ssl_cert():
+    logstash_ssl_cert = config().get('logstash_ssl_cert')
+    logstash_ssl_key = config().get('logstash_ssl_key')
+    if logstash_ssl_cert and logstash_ssl_key:
+        render(template='{{ data }}',
+               context={'data': base64.b64decode(logstash_ssl_cert)},
+               target=LOGSTASH_SSL_CERT, perms=0o444)
+        render(template='{{ data }}',
+               context={'data': base64.b64decode(logstash_ssl_key)},
+               target=LOGSTASH_SSL_KEY, perms=0o400)
+    else:
+        if not logstash_ssl_cert and os.path.exists(LOGSTASH_SSL_CERT):
+            os.remove(LOGSTASH_SSL_CERT)
+        if not logstash_ssl_key and os.path.exists(LOGSTASH_SSL_KEY):
+            os.remove(LOGSTASH_SSL_KEY)
 
 
 @when('apt.installed.filebeat')
